20e976bdc63be64cfa56846718d025497d933a8c
mislavkovac <mkovac07@fesb.hr>
2021-10-23 10:38:31 +0200

    Adding logo

--- modules/header/header.js
+++ modules/header/header.js
@@ -1,17 +1,23 @@
 import React from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
+import LogoImg from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <header>
-      <div>Logo</div>
-      <nav>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Showcase</li>
-        <li>Blog</li>
-        <li>Contact</li>
-        <li>Sign In</li>
-      </nav>
+    <header className={styles.main}>
+      <main>
+        <div>Logo</div>
+        <Image src={LogoImg} alt="logo" />
+        <nav className={styles.navigation}>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Showcase</li>
+          <li>Blog</li>
+          <li>Contact</li>
+          <li>Sign In</li>
+        </nav>
+      </main>
     </header>
   );
 };
